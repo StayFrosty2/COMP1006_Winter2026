a5044b8f311c98598b9b2e5ff223da2914189435
Claire <cameron.liam.yon@gmail.com>
2026-01-23 14:08:09 -0500

    fixed a couple extra small things in 03 02

--- 03/02_fix_this_code/index.php
+++ 03/02_fix_this_code/index.php
@@ -10,12 +10,12 @@ $dbname = "week_three";
 $username = "root";
 $password = "";
 
-$dsn = "mysql:host=$host dbname=$dbname";
+$dsn = "mysql:host=$host; dbname=$dbname";
 
 try {
     
     $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connected to database!";
 }
