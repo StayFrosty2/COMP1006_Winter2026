4b3fc373a1ea5368e4bcd6bd1c39d2bd788e3352
Claire <cameron.liam.yon@gmail.com>
2026-01-23 14:51:12 -0500

    lab 2 complete i think

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -7,31 +7,38 @@
     How can we refactor this code so it’s easier to maintain?
 */
 
-$items = ["Home", "About", "Contact"];
-
+/* 
+    First, move the items into a seperate section. Including them at the top like this is convient, but becomes tedious. I moved them to
+    a seperate PHP file alongside the navigation, as they are only used for that one thing, and will be included on other pages as well.
+*/
 ?>
 
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <title>My PHP Page</title>
-    </head>
+<head>
+    <title>My PHP Page</title>
+</head>
 
-    <body>
+<body>
+    <!--
+        The entire header is now a couple lines in this document. The H1 and Navigation would be included on every page of this website, so it
+        makes sense for them to be easy to incorporate on every page. What's more is that it's now easier to edit them as the page gains more
+        and more content, and is divided up into further sections.
+    -->
 
-        <h1>Welcome</h1>
+    <?php
+    include "nav.php";
+    ?>
 
-        <ul>
-            <?php
-                include "nav.php";
-            ?>
-        </ul>
+    <main>
+        <!-- CONTENT WOULD GO HERE -->
+    </main>
 
-        <footer>
-            <p>&copy; 2026</p>
-        </footer>
+    <footer>
+        <p>&copy; 2026</p>
+    </footer>
 
-    </body>
+</body>
 
 </html>
--- 03/03_make_it_maintainable/nav.php
+++ 03/03_make_it_maintainable/nav.php
@@ -1,3 +1,18 @@
-<?php foreach ($items as $item): ?>
-<li><?= $item ?></li>
-<?php endforeach; ?>
+<header>
+    <h1> Welcome! </h1>
+
+    <nav>
+        <ul>
+            <?php
+            // The would-be clickable links were this an actual websites navigation menu
+            $items = ["Home", "About", "Contact"];
+
+
+            // Includes the following HTML after the foreach in the document to build the website's navigation menu
+            foreach ($items as $item):
+            ?>
+                <li><?= $item ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+</header>
