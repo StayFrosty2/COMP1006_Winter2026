e6b3ecd19860bc2faf29f78c2f8e813fc5015619
Claire <cameron.liam.yon@gmail.com>
2026-01-30 15:57:53 -0500

    contact php now uses a text area, working on validation for names and email

--- assignments/labs/lab_three/contact.php
+++ assignments/labs/lab_three/contact.php
@@ -4,15 +4,14 @@
     <form action="submit.php" method="post">
         <!-- User Information -->
         <fieldset>
-            <legend>Customer Information</legend>
             <label for="first_name">First Name:</label>
-            <input type="text" id="first_name" name="first_name">
+            <input type="text" id="first_name" name="first_name" required>
             <label for="last_name">Last Name:</label>
-            <input type="text" id="last_name" name="last_name" >
+            <input type="text" id="last_name" name="last_name" required>
             <label for="email">Email Address:</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" required>
             <label for="message">Write your message here:</label>
-            <input type="text" id="message" name="message">
+            <textarea id="message" name="message" rows="5" minlength="10" required></textarea>
         </fieldset>
         <button type="submit">Submit</button>
         <button type="reset">Reset</button>
--- assignments/labs/lab_three/submit.php
+++ assignments/labs/lab_three/submit.php
@@ -1,4 +1,29 @@
 <?php 
+    include "includes/header.php";
+
     // Get information from user and sanitize
+    $firstName = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_SPECIAL_CHARS);
+    $lastName = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Validation - Serverside
+
+    $errors[] = [];
+
+    // Ensure First and Last Name have a value
+
+    if($firstName == null || $firstName == "") {
+        $errors[] = "First Name is Required.";
+    }
+    if($lastName == null || $lastName == "") {
+        $errors[] = "Last Name is Required.";
+    }
+
+    // Ensure Email has a value, and is a valid email
+
+    if($email == null || $email == "") {
+        $errors[] = "Email Address is required";
+    }
 
 ?>
