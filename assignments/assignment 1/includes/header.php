533aa71cb36c86727b30e8e80d447c28ab57ac1b
Claire <cameron.liam.yon@gmail.com>
2026-02-12 17:15:54 -0500

    started the website

---
+++ assignments/assignment 1/includes/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="author" content="Cameron Yon-Vachon" />
+        <link rel="stylesheet" href="styles/styles.css" />
---
+++ assignments/assignment 1/index.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php"); ?>
+        <meta name="description" content="Cameron's Blog Website | Homepage" />
+        <title>Cameron's Blog Website | Homepage</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Cameron's Blog Website</h1>
+            <?php include("includes/nav.php"); ?>
+        </header>
+        <main>
+            
+        </main>
+    </body>
+<?php include("includes/footer.php"); ?>
