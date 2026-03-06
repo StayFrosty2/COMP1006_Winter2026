1f8aa3ee37647158bdd778b23d2ff470807e4d82
Claire <cameron.liam.yon@gmail.com>
2026-03-06 13:02:21 -0500

    started delete.php

--- Midterm_COMP1006_Winter2026_v3-main/delete.php
+++ Midterm_COMP1006_Winter2026_v3-main/delete.php
@@ -3,3 +3,17 @@
 // Connects to the database
 require("connect.php");
 
+// Check if an id is set, if not, send the user back to the homepage
+if(!isset($_GET["id"])) {
+    header("Location: index.php");
+    exit;
+}
+
+// Get the id from the url
+$id = $_GET["id"];
+
+// create and run an sql script to delete the post from the server
+$sql = "DELETE FROM book_manager.reviews WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+$stmt->execute();
