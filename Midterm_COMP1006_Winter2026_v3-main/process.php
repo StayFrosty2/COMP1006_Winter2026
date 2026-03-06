f81cd5b2849b1254e23a7f8b93ed26fcaf8e9f23
Claire <cameron.liam.yon@gmail.com>
2026-03-06 12:31:27 -0500

    data grabbed and sanitized

--- Midterm_COMP1006_Winter2026_v3-main/process.php
+++ Midterm_COMP1006_Winter2026_v3-main/process.php
@@ -2,6 +2,20 @@
     // Connects to the database
     require("connect.php");
 
+    // If the server request method is not post, kill the page
+    if($_SERVER["REQUEST_METHOD"] !== "POST") {
+        die("Invalid Request");
+    }
+
+    // Grab form data and sanitize
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_SPECIAL_CHARS);
+    $author = filter_input(INPUT_POST, "author", FILTER_SANITIZE_SPECIAL_CHARS);
+    $rating = filter_input(INPUT_POST, "rating", FILTER_SANITIZE_NUMBER_INT);
+    $text = filter_input(INPUT_POST, "review_text", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Empty array to catch errors
+    $errors = [];
+
     
 
 ?>
