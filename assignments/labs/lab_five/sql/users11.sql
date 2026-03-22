b25b042e999aff018dabcf9e3cb0ee33b9607022
Claire <cameron.liam.yon@gmail.com>
2026-03-22 17:46:10 -0400

    updated sql

--- assignments/labs/lab_five/sql/users11.sql
+++ assignments/labs/lab_five/sql/users11.sql
@@ -1,6 +1,8 @@
 create table users11 (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id     INT AUTO_INCREMENT PRIMARY KEY,
     
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    username    VARCHAR(100) NOT NULL,
+    password    VARCHAR(255) NOT NULL,
+    image_path  VARCHAR(255),
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
  );
